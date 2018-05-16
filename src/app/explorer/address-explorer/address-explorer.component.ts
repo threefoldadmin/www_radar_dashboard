9cd6ba28686c2f26b8bc49e3ad5846133f9debd6
Ksenia Abakumova <k.s.abakumova@gmail.com>
2018-05-16 12:14:17 +0500

    + | address-explorer | ready for testing

--- src/app/explorer/address-explorer/address-explorer.component.ts
+++ src/app/explorer/address-explorer/address-explorer.component.ts
@@ -11,6 +11,15 @@ export class AddressExplorerComponent implements OnInit {
   @Input() public item;
   @Input() public id;
 
+  public paginator = {
+    limit: 10,
+    current: {
+      transactions: 1,
+      minerPayouts: 1,
+      blockStakeMotion: 1
+    }
+  };
+
   constructor(
     private router: Router,
     private appComponent: AppComponent,
@@ -33,62 +42,14 @@ export class AddressExplorerComponent implements OnInit {
   public currentCurrencyPair() {
     return this.appComponent.currentCurrencyPair;
   }
-  public isAddressCoinOuputs(transactions: Array<any>) {
-    let result = false;
-    if (transactions !== null) {
-      transactions.map((transaction, i) => {
-        if (transaction.coinoutputids != null && transaction.coinoutputids.length !== 0) {
-          transaction.coinoutputids.map((coinoutputid, j) => {
-            if (transaction.rawtransaction.data.coinoutputs[j].unlockhash === this.id) {
-              result = true;
-            }
-          });
+  public pageChanged(page: number, name: string) {
+    this.paginator.current[name] = page;
+    this.appComponent.getPageData('wallet', this.item._id, this.paginator.limit, this.paginator.current[name], name).subscribe(
+      data => {
+        if (data) {
+          this.item[name] = data;
         }
-      });
-    }
-    return result;
-  }
-  public isAddressMinerPayouts(item: any) {
-    let result = false;
-    if (item.blocks != null && item.blocks.length !== 0) {
-      item.blocks.map((block, i) => {
-        block.minerpayoutids.map((minerpayoutid, j) => {
-          if (block.rawblock.minerpayouts[j].unlockhash === this.id) {
-            result = true;
-          }
-        });
-      });
-    }
-    return result;
-  }
-  public isAddressBlockstakeOutputs(transactions: Array<any>) {
-    let result = false;
-    if (transactions !== null) {
-      transactions.map((transaction, i) => {
-        const blockstakeoutputids = transaction.blockstakeoutputids;
-        if (blockstakeoutputids != null && blockstakeoutputids.length !== 0) {
-          blockstakeoutputids.map((blockstakeoutputid, j) => {
-            if (transaction.rawtransaction.data.blockstakeoutputs[j].unlockhash === this.id) {
-              result = true;
-            }
-          });
-        }
-      });
-    }
-    return result;
-  }
-  public hasBeenSpent(transactions: any, value: string, typeName: string) {
-    let result = 'No';
-    const type = typeName === 'coin' ? 'coininputoutputs' : 'blockstakeinputoutputs';
-    transactions.map(transaction => {
-      if (transaction[type] !== null) {
-        transaction[type].map((el) => {
-          if (el.value === value && el.condition.data.unlockhash === this.id) {
-            result = 'Yes';
-          }
-        });
       }
-    });
-    return result;
+    );
   }
 }
